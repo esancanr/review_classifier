2e33218928891003e8b3a002af0c02cc2dc77401
rmiguel <e.eduardo.s234@gmail.com>
2026-09-04 10:09:25 -0500

    first commit

---
+++ discovery_data.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+# ============================================================
+# 1. CARGAR EL DATASET
+# ============================================================
+data = pd.read_csv('data/IMDB Dataset.csv')
+
+# 1.1. REVISAR LA CANTIDAD DE DATOS POR CADA SENTIMIENTO
+# Esta línea cuenta cuántas reseñas existen de cada categoría:
+# "positive" y "negative".
+# data_count_balance = data.value_counts('sentiment')
+# print(data_count_balance)
+
+# ============================================================
+# 2. CREAR UN DATASET DESBALANCEADO
+# ============================================================
+df_postive = data[data['sentiment'] == 'positive'][:9000]
+df_negative = data[data['sentiment'] == 'negative'][:1000]
+
+# 2.1 Unir las reseñas positivas y negativas en un solo DataFrame.
+df_review_des = pd.concat([df_postive, df_negative])
+
+# 2.2. REVISAR EL DATASET DESBALANCEADO
+# Estas líneas permiten comprobar cuántas reseñas hay
+# de cada categoría después de crear el dataset desbalanceado.
+# data_count_des = df_review_des.value_counts('sentiment')
+# print(data_count_des)
+
+# ============================================================
+# 3. BALANCEAR EL DATASET
+# ============================================================
+rus = RandomUnderSampler()
+df_review_bal, df_review_bal['sentiment'] = rus.fit_resample(
+    df_review_des[['review']], 
+    df_review_des['sentiment']
+    )
+print(df_review_bal)
+data_count_balance = df_review_bal.value_counts('sentiment')
+print(data_count_balance)
+
+# ============================================================
+# 6. DIVIDIR LOS DATOS EN ENTRENAMIENTO Y PRUEBA
+# ============================================================
+train, test = train_test_split(df_review_bal, test_size=0.33, random_state=42)
+train_x, train_y = train['review'], train['sentiment']
+test_x, test_y = test['review'], test['sentiment']
+
+# ============================================================
+# 7. CONVERTIR EL TEXTO EN NÚMEROS
+# ============================================================
+
+# Los modelos de Machine Learning no pueden trabajar directamente
+# con texto.
+#
+# Por eso utilizamos TF-IDF, que transforma las palabras de las
+# reseñas en valores numéricos.
+
+tfidf = TfidfVectorizer(stop_words='english')
+train_x_vector = tfidf.fit_transform(train_x)
+test_x_vector = tfidf.transform(test_x)
